2d036e7605c42ab48d88426dc7220e4e08ebcaca
dipesh shakya <dipeshshakya106@gmail.com>
2020-08-13 16:17:37 +1000

    navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 function Navbar() {
   return (
     <div>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/Shop">Shop</Link>
-        <Link to="/Checkout">Checkout</Link>
-        <Link to="/Product">Product</Link>
-      </ul>
+      <AppBar position="static">
+        <Toolbar className="navbar__link">
+          <Link to="/">Home</Link>
+          <Link to="/Shop">Shop</Link>
+          <Link to="/Checkout">Checkout</Link>
+          <Link to="/Product">Product</Link>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
